b499459e754994f82e837c4ba855b0a1f1b3bf03
Viraj Srivastav <virajsrivastav1112@gmail.com>
2026-07-04 21:03:59 +0530

    feat: generate and configure brand logo icons and favicons

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -64,6 +64,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../favicon.ico'),
+        to: ''
       }
     ])
   ]
